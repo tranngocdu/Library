792400b1fd414edfdd04f55071350f88031af288
hellogrid <hello@hellogrid.com>
2013-11-05 18:48:55 -0600

    student list check added

--- content/public/javascripts/app.js
+++ content/public/javascripts/app.js
@@ -2331,7 +2331,7 @@ window.require.register("views/templates/studentListCheck", function(exports, re
   function program1(depth0,data) {
     
     var buffer = "", stack1;
-    buffer += "\n    <li data-id=\"";
+    buffer += "\n		<li data-id=\"";
     if (stack1 = helpers.objectId) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.objectId; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
@@ -2343,14 +2343,14 @@ window.require.register("views/templates/studentListCheck", function(exports, re
     if (stack1 = helpers.Name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.Name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + "</li>\n";
+      + "<div class=\"student-check\"></div></li>\n	";
     return buffer;
     }
 
-    buffer += "  <ul id=\"studentlist\">\n";
+    buffer += "<ul id=\"studentlist\">\n	";
     stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
     if(stack1 || stack1 === 0) { buffer += stack1; }
-    buffer += "\n\n  </ul>\n\n";
+    buffer += "\n</ul>";
     return buffer;
     });
 });
