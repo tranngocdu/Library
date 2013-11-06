c5cd561829a56e3059f1103aea014cc2a9c6719e
hellogrid <hello@hellogrid.com>
2013-11-05 20:03:48 -0600

    bigger font sizes on book titles and authors

--- iOS/www/javascripts/app.js
+++ iOS/www/javascripts/app.js
@@ -1073,7 +1073,7 @@ window.require.register("views/checkin-view", function(exports, require, module)
   			$(e.currentTarget).addClass("selected");
 
   			$("#checkIn").removeClass("disabled");
-  			$("#checkIn").addClass("primary-fill");
+  			$("#checkIn").addClass("secondary");
   		}
   		//Just highlite the damn thing already
   		else {
@@ -1083,12 +1083,12 @@ window.require.register("views/checkin-view", function(exports, require, module)
   			$(e.currentTarget).removeClass("deselected");
   			$(e.currentTarget).addClass("selected");
   			$("#checkIn").removeClass("disabled");
-  			$("#checkIn").addClass("primary-fill");
+  			$("#checkIn").addClass("secondary");
   		};
   		//If a name isn't selected make sure the Check Out button isn't highlited
   		if (!$(".studentCheck").hasClass("selected")){
   			$("#checkIn").addClass("disabled");
-  			$("#checkIn").removeClass("primary-fill");
+  			$("#checkIn").removeClass("secondary");
   		};
 
 
@@ -1950,7 +1950,7 @@ window.require.register("views/templates/addBookManually", function(exports, req
     var buffer = "";
 
 
-    buffer += "<div id=\"header\">\n	<div class=\"back\">Cancel</div>\n  \n  <h1>Add Book</h1>\n</div>\n\n<div id=\"wrapper\">\n  <div id=\"scroller\" class=\"container\">\n	\n		<div class=\"no-art\">\n			<div class=\"no-icon\"></div>\n		</div>\n	\n	<div id=\"addPhoto\" class=\"button primary\">Add Photo</div>\n    \n    <input id=\"title\" type=\"text\" placeholder=\"Book Title\" />\n    <input id=\"author\" type=\"text\" placeholder=\"Book Author\" />\n    <input id=\"numberAvailable\" type=\"text\" placeholder=\"Quantity Available\" />\n\n    <div id=\"addBook\" class=\"button primary\">Add Book</div>\n\n\n  </div> "
+    buffer += "<div id=\"header\">\n	<div class=\"back\">Cancel</div>\n  \n  <h1>Add Book</h1>\n</div>\n\n<div id=\"wrapper\">\n  <div id=\"scroller\" class=\"container add-scroll\">\n    <div id=\"custom-art\"><img src=\"\"></div>\n		<div class=\"no-icon\"></div>\n    <div id=\"addPhoto\" class=\"button sm-btn secondary\">Add Photo</div>\n\n    <input id=\"title\" class=\"first-input\" type=\"text\" placeholder=\"Book Title\" />\n    <input id=\"author\" type=\"text\" placeholder=\"Book Author\" />\n    <input id=\"numberAvailable\" type=\"text\" placeholder=\"Quantity Available\" />\n\n    <div id=\"addBook\" class=\"button primary-fill\">Add Book</div>\n\n\n  </div> "
       + "\n</div> "
       + "\n\n";
     return buffer;
@@ -2131,21 +2131,21 @@ window.require.register("views/templates/checkIn", function(exports, require, mo
     buffer += "\n		";
     stack1 = helpers['if'].call(depth0, depth0.cover_image, {hash:{},inverse:self.program(4, program4, data),fn:self.program(2, program2, data),data:data});
     if(stack1 || stack1 === 0) { buffer += stack1; }
-    buffer += "\n\n<div class=\"title-info\">\n	<h2 class=\"truncate-three\">";
+    buffer += "\n\n	<div class=\"title-info\">\n		<h2 class=\"truncate-three\">";
     if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + "</h2>\n	<h3 class=\"truncate\">";
+      + "</h2>\n		<h3 class=\"truncate\">";
     if (stack1 = helpers.author) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.author; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + "</h3>\n	"
-      + "\n	<p>";
+      + "</h3>\n		"
+      + "\n		<p>";
     if (stack1 = helpers.quantity_available) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.quantity_available; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + " Available</p>\n	<div id=\"checkIn\" class=\"check-btn button disabled\">Check Out</div> "
-      + "\n</div>\n";
+      + " Available</p>\n		<div id=\"checkIn\" class=\"check-btn button disabled\">Check In</div> "
+      + "\n	</div>\n	";
     return buffer;
     }
   function program2(depth0,data) {
@@ -2168,8 +2168,9 @@ window.require.register("views/templates/checkIn", function(exports, require, mo
     buffer += "<div id=\"header\">\n	<div class=\"back\">Books</div>\n	<h1>Check In</h1>\n</div>\n\n<div class=\"check\">\n	";
     stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
     if(stack1 || stack1 === 0) { buffer += stack1; }
-    buffer += "\n\n<div class=\"clearfix\"></div>\n\n<div id=\"wrapper\" class=\"check-wrap\">\n	<div class=\"name-header\">Pick your name</div>\n\n	<div id=\"scroller\" class=\"students\">\n		\n	</div> "
-      + "\n\n</div> ";
+    buffer += "\n\n</div> "
+      + "\n<div class=\"clearfix\"></div>\n\n<div class=\"name-header\">Pick your name</div>\n\n<div id=\"wrapper\" class=\"check-wrap\">\n	<div id=\"scroller\" class=\"students\">\n	</div> "
+      + "\n</div> ";
     return buffer;
     });
 });
@@ -2185,21 +2186,21 @@ window.require.register("views/templates/checkOut", function(exports, require, m
     buffer += "\n		";
     stack1 = helpers['if'].call(depth0, depth0.cover_image, {hash:{},inverse:self.program(4, program4, data),fn:self.program(2, program2, data),data:data});
     if(stack1 || stack1 === 0) { buffer += stack1; }
-    buffer += "\n\n<div class=\"title-info\">\n	<h2 class=\"truncate-three\">";
+    buffer += "\n\n	<div class=\"title-info\">\n		<h2 class=\"truncate-three\">";
     if (stack1 = helpers.title) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.title; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + "</h2>\n	<h3 class=\"truncate\">";
+      + "</h2>\n		<h3 class=\"truncate\">";
     if (stack1 = helpers.author) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.author; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + "</h3>\n	"
-      + "\n	<p>";
+      + "</h3>\n		"
+      + "\n		<p>";
     if (stack1 = helpers.quantity_available) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
     else { stack1 = depth0.quantity_available; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
     buffer += escapeExpression(stack1)
-      + " Available</p>\n	<div id=\"checkOut\" class=\"check-btn button disabled\">Check Out</div> "
-      + "\n</div>\n";
+      + " Available</p>\n		<div id=\"checkOut\" class=\"check-btn button disabled\">Check Out</div> "
+      + "\n	</div>\n	";
     return buffer;
     }
   function program2(depth0,data) {
@@ -2222,7 +2223,8 @@ window.require.register("views/templates/checkOut", function(exports, require, m
     buffer += "<div id=\"header\">\n	<div class=\"back\">Books</div>\n	<h1>Check Out</h1>\n</div>\n\n<div class=\"check\">\n	";
     stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
     if(stack1 || stack1 === 0) { buffer += stack1; }
-    buffer += "\n\n<div class=\"clearfix\"></div>\n\n<div id=\"wrapper\" class=\"check-wrap\">\n	<div class=\"name-header\">Pick your name</div>\n	<div id=\"scroller\" class=\"students\">\n	</div> "
+    buffer += "\n\n</div> "
+      + "\n<div class=\"clearfix\"></div>\n\n<div class=\"name-header\">Pick your name</div>\n\n<div id=\"wrapper\" class=\"check-wrap\">\n	<div id=\"scroller\" class=\"students\">\n	</div> "
       + "\n</div> ";
     return buffer;
     });
