b48b88600698139974b5d58cd270685a4a587cfa
Albert Swantner <aswantner@gmail.com>
2014-02-25 11:50:36 -0600

    student book list

--- content/app/views/student-view.js
+++ content/app/views/student-view.js
@@ -1,11 +1,14 @@
 var View = require('./view');
 var template = require('./templates/student');
+var templateBooks = require('./templates/bookListBooks');
 
 module.exports = View.extend({
 	id: 'student-view',
 	template: template,
+	templateBooks:templateBooks,
 	events: {
-
+		'click #filt-current':'currentBooks',
+		'click #filt-past':'pastBooks',
 	},
 
 	initialize: function() {
@@ -13,14 +16,37 @@ module.exports = View.extend({
 	},
 
 	render: function() {
-		this.$el.html(this.template());
 		var that = this;
+		that.currentBookArray = {};
+		that.pastBookArray = {};
+		this.$el.html(this.template());
 		var currentUser = Parse.User.current();
 		console.log(currentUser);
 		var currentUserId = currentUser.id;
 		var query = new Parse.Query("Student");
+		query.equalTo("objectId", this.id);
+		query.first({
+			success:function(student) {
+				var bookArray = JSON.stringify(student);
+				var bookArray = JSON.parse(bookArray);
+				that.bookArray = bookArray;	
+				that.currentBookArray = that.bookArray.currentBooks;
+				that.pastBookArray = that.bookArray.pastBooks;
+				$('#wrapper').html(that.templateBooks(that.currentBooksArray));
+			}
+		})
 
 		return this;
+	},
+	
+	currentBooks: function() {
+		var that = this;
+		$('#wrapper').html(that.templateBooks(that.currentBookArray));
+	},
+	
+	pastBooks: function() {
+		var that = this;
+		$('#wrapper').html(that.templateBooks(that.pastBookArray));
 	}
 
 });
