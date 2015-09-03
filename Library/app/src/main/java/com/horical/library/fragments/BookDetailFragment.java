18fe0019da11516ac6f3672045f3d8dbecd2d451
vanan <vananit93@gmail.com>
2015-09-03 14:23:31 +0700

    update book_detail

--- Library/app/src/main/java/com/horical/library/fragments/BookDetailFragment.java
+++ Library/app/src/main/java/com/horical/library/fragments/BookDetailFragment.java
@@ -6,14 +6,35 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.horical.library.R;
+import com.horical.library.adapters.BookDetailNameUserAdapter;
 import com.horical.library.bases.BaseFragment;
+import com.horical.library.dto.Book;
+import com.horical.library.dto.BookDetail;
 
-/**
- * Created by Van An on 8/28/2015.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import static android.R.layout.simple_list_item_1;
+
+
 public class BookDetailFragment extends BaseFragment implements View.OnClickListener {
+
+    ListView mListView, mListViewIndex;
+    BookDetailNameUserAdapter mBookDetailNameUserAdapter;
+    ArrayAdapter<String> mIndexAdapter;
+    LinearLayout sideIndex;
+    String[] mStringIndex;
+    List<String> mIndexListName= new ArrayList<>();
+    Vector<Book> mBookDetailVector;
+    List<Integer> mDealList = new ArrayList<>();
+    int mTotalListSize=0;
 
     public static BookDetailFragment newInstance() {
         return new BookDetailFragment();
@@ -33,7 +54,67 @@ public class BookDetailFragment extends BaseFragment implements View.OnClickList
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book_detail, container, false);
+        mListView = (ListView) view.findViewById(R.id.lvBookName);
+        mListViewIndex= (ListView) view.findViewById(R.id.lvIndex);
+        mBookDetailVector = BookDetail.getBookList();
+        Vector subsidiesList=getIndexedBooks(mBookDetailVector);
+        mTotalListSize = subsidiesList.size();
+        mBookDetailNameUserAdapter= new BookDetailNameUserAdapter(getActivity(), subsidiesList);
+        setStringIndex();
+        mIndexAdapter=new ArrayAdapter<>(getActivity(), simple_list_item_1, mStringIndex);
+        mListView.setAdapter(mBookDetailNameUserAdapter);
+        sideIndex = (LinearLayout) view.findViewById(R.id.llSlideIndex);
+        mListViewIndex.setAdapter(mIndexAdapter);
+        mListViewIndex.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                displayListItem(position);
+            }
+        });
         return view;
+    }
+
+    public void setStringIndex(){
+        mStringIndex= new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","T","U","V","X","Y","Z"};
+    }
+
+    private Vector<Book> getIndexedBooks(Vector<Book> booksVector) {
+        Vector<Book> v = new Vector<>();
+        String idx1;
+        String idx2 = null;
+        for (int i = 0; i < booksVector.size(); i++) {
+            Book temp = booksVector.elementAt(i);
+            idx1 = (temp.getName().substring(0, 1)).toLowerCase();
+            if (!idx1.equalsIgnoreCase(idx2)) {
+                v.add(new Book(idx1.toUpperCase(), "", "", "", ""));
+                idx2 = idx1;
+                mIndexListName.add(idx1.toLowerCase().toString());
+                mDealList.add(i);
+            }
+            v.add(temp);
+        }
+        return v;
+    }
+
+    public int getPosition(String s, List<String> strings){
+        int pos=0;
+        for (int i=0; i<strings.size();i++){
+            if (strings.get(i).equals(s))
+            {
+                pos=i;
+            }
+        }
+        return pos;
+    }
+
+    public void displayListItem(int itemPosition) {
+        int pos=getPosition(mStringIndex[itemPosition].toLowerCase(), mIndexListName);
+        int listLocation = mDealList.get(pos);
+
+        if (listLocation > mTotalListSize) {
+            listLocation = mTotalListSize;
+        }
+        mListView.setSelection(listLocation);
     }
 
     @Override
